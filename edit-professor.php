f3a79af7f5f9269a0872421a3ee1ef6b6c39606d
pavle95 <pavlecalov@gmail.com>
2018-03-16 10:36:16 +0100

    fixed edit professor/student/subject/user

--- edit-professor.php
+++ edit-professor.php
@@ -6,18 +6,21 @@
     <title>Title</title>
 </head>
 <body>
-<script src="js-scripts.js"></script>
+<script src="validation-professor.js"></script>
         <?php
         $id = $_REQUEST['id'];
-        $sql = "SELECT * from professor where id=".$id;
+        $sql = "SELECT p.*, u.*  from professor p, users u where p.id = u.professor_id and p.id=".$id;
         $result = $conn->query($sql);
-        $firstname;$lastname;$age;$subject;
+        $firstname;$lastname;$age;$subject;$username;$password;$email;$admin;
         if($result->num_rows>0) {
             while ($row = $result->fetch_assoc()) {
                 $firstname = $row['first_name'];
                 $lastname = $row['last_name'];
                 $age = $row['age'];
                 $subject = $row['subject_id'];
+                $username = $row['username'];
+                $password = $row['password'];
+                $email = $row['email'];
             }
         }
         ?>
@@ -29,6 +32,14 @@
                 $age = $_POST['age'];
                 $subject = $_POST['subject'];
                 $sql = "UPDATE professor SET first_name='".$firstname."',last_name='".$lastname."',age=".$age.",subject_id=".$subject." WHERE id=".$id;
+                if ($conn->query($sql) == false) {
+                    echo $sql;
+                }
+                $email =$_POST['email'];
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $admin = 0;
+                $sql = "UPDATE users SET email='$email', username='$username', password='$password', is_admin='$admin' where professor_id=".$id;
                 if ($conn->query($sql) == true) {
                     echo '<script type="text/javascript"> window.location = "professor-list.php"</script>';
                 }else{
@@ -36,6 +47,15 @@
                 }
             }
             ?>
+            Email:<br>
+            <input type="text" name="email"  value="<?php echo $email ?>"><br>
+
+            Username:<br>
+            <input type="text" name="username"  value="<?php echo $username ?>"><br>
+
+            Password:<br>
+            <input type="password" name="password"  value="<?php echo $password ?>"><br>
+
             First name:<br>
             <input type="text" name="firstname" id="firstname" value="<?php echo $firstname ?>"><br>
 
@@ -48,7 +68,7 @@
             Subject:<br>
             <select name="subject" id="subject">
                 <?php
-                $sql = "select * from subject";
+                $sql = "select * from subject where id not in (select subject_id from professor where id not like '$id')";
                 $result = $conn->query($sql);
                 if($result->num_rows>0) {
                     while ($row = $result->fetch_assoc()) {
